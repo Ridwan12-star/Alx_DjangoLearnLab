a7c215bea1558cf732e00d2f6b3f33124198ca77
Ridwan12-star <issahgh33@gmail.com>
2025-07-25 19:36:03 -0400

    sss

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -20,6 +20,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth import login
 
 # Register View
 def register_view(request):
