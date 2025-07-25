525ca6d5e36cf764d23a0e9da561efc4571e40d9
Ridwan12-star <issahgh33@gmail.com>
2025-07-25 19:07:21 -0400

    Initial commit: Django models project

---
+++ django-models/LibraryProject/bookshelf/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Book
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')  # Shows these fields in the admin list view
+    search_fields = ('title', 'author')                     # Enables search by title and author
+    list_filter = ('publication_year',)                     # Adds a filter by year on the side
---
+++ django-models/LibraryProject/bookshelf/create.md
@@ -0,0 +1,10 @@
+# 📘 Create Operation
+
+```python
+from bookshelf.models import Book
+
+# Create a new book instance
+book = Book.objects.create(title="1984", author="George Orwell", publication_year=1949)
+book
+# Output: <Book: 1984 by George Orwell (1949)>
+```
