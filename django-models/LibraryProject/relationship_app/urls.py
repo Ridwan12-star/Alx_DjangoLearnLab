a0aa4ee7c634debfb6048d47dae90a4771e7fd26
Ridwan12-star <issahgh33@gmail.com>
2025-07-25 19:51:30 -0400

    ooo

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -30,3 +30,11 @@ urlpatterns = [
     # Registration using custom view
     path('register/', views.register, name='register'),
 ]
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('admin-role/', views.admin_view, name='admin_view'),
+    path('librarian-role/', views.librarian_view, name='librarian_view'),
+    path('member-role/', views.member_view, name='member_view'),
+]
